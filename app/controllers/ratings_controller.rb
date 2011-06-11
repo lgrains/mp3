13e8789ab0473c2192737a4cf8eb29703d994320
Louise Rains <lrains@backchannelmedia.com>
2011-06-11 13:55:14 -0400

    Removed unneeded methods from RatingsController

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,28 +1,6 @@
 class RatingsController < ApplicationController
-  # GET /ratings
-  # GET /ratings.xml
-  def index
-    @ratings = Rating.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @ratings }
-    end
-  end
 
-  # GET /ratings/1
-  # GET /ratings/1.xml
-  def show
-    @rating = Rating.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @rating }
-    end
-  end
-
-  # GET /ratings/new
-  # GET /ratings/new.xml
   def new
     @rating = Rating.new
 
@@ -32,13 +10,7 @@ class RatingsController < ApplicationController
     end
   end
 
-  # GET /ratings/1/edit
-  # def edit
-  #     @rating = Rating.find(params[:id])
-  #   end
 
-  # POST /ratings
-  # POST /ratings.xml
   def create
     @rating = Rating.new(params[:rating])
     @rating.mp3tune_id = params[:mp3tune_id]
@@ -52,31 +24,5 @@ class RatingsController < ApplicationController
     end
   end
 
-  # PUT /ratings/1
-  # PUT /ratings/1.xml
-  # def update
-  #     @rating = Rating.find(params[:id])
-  # 
-  #     respond_to do |format|
-  #       if @rating.update_attributes(params[:rating])
-  #         format.html { redirect_to(@rating, :notice => 'Rating was successfully updated.') }
-  #         format.xml  { head :ok }
-  #       else
-  #         format.html { render :action => "edit" }
-  #         format.xml  { render :xml => @rating.errors, :status => :unprocessable_entity }
-  #       end
-  #     end
-  #   end
-  
-  # DELETE /ratings/1
-  # DELETE /ratings/1.xml
-  # def destroy
-  #     @rating = Rating.find(params[:id])
-  #     @rating.destroy
-  #
-  #     respond_to do |format|
-  #       format.html { redirect_to(ratings_url) }
-  #       format.xml  { head :ok }
-  #     end
-  #   end
+
 end
